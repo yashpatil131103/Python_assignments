1fe4b3648214121780ab8ebee77f4fc3f2c57abb
yashpatil131103 <131103yashpatil@gmail.com>
2025-11-21 00:05:52 +0530

    raminig morning

---
+++ if-else9.py
@@ -0,0 +1,18 @@
+#9.	Write a program to calculate the electricity bill (accept number of unit from user) according to the following criteria :  
+#             Unit                                    Price    
+#First 100 units                                       no charge  
+#Next 100 units                                        Rs 5 per unit  
+#After 200 units                                       Rs 10 per unit  
+#(For example if input unit is 350 than total bill amount is Rs2000)  
+
+units = int(input("Enter electricity units consumed: "))
+
+if units <= 100:
+    bill = 0
+elif units <= 200:
+    bill = (units - 100) * 5
+else:
+    bill = (100 * 5) + (units - 200) * 10
+
+print("Total bill amount: Rs", bill)
+
